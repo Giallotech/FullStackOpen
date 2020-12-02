0e59b54b10807dd48feef2fd3c03ac0a92658be9
Gianluca <giallotech@gmail.com>
2020-12-02 19:47:10 +0100

    I completed some new exercises and made some changes

---
+++ part_2/phonebook_step4/src/index.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [searchName, setSearchName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(nameObject))
+    uniqueArr.forEach(value => {
+      if (value.name === newName) {
+        alert(`${newName} is already added to phonebook`);
+      }
+    })
+    setNewName('')
+    setNewNumber('')
+  }
+  const uniqueArr = [...new Map(persons.map(item => [item.name, item])).values()]
+  const searchresults = uniqueArr.filter(person =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleSearch= (event) => {
+    setSearchName(event.target.value)
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>filter shown with <input 
+      type="text" 
+      placeholder="Search" 
+      value={searchName} 
+      onChange={handleSearch}></input></div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul style= {{display:"table-row"}}>
+        {searchresults.map(item => (
+          <li key={item.name}>
+            {item.name} {item.number}
+            </li>
+          ))}        
+      </ul>
+    </div>
+  )
+}
+
+export default App
+ReactDOM.render(<App />, document.getElementById("root"));
+
---
+++ part_2/phonebook_step5/src/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react'
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [searchName, setSearchName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(nameObject))
+    uniqueArr.forEach(value => {
+      if (value.name === newName) {
+        alert(`${newName} is already added to phonebook`);
+      }
+    })
+    setNewName('')
+    setNewNumber('')
+  }
+  const uniqueArr = [...new Map(persons.map(item => [item.name, item])).values()]
+  const searchresults = uniqueArr.filter(person =>
+    person.name.toLowerCase().includes(searchName.toLowerCase())
+  );
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleSearch= (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const Filter = (props) => {
+    return (
+      <div>filter shown with <input 
+      type="text" 
+      placeholder="Search" 
+      value={props.searchName} 
+      onChange={props.handleSearch}></input></div>
+    )
+  }
+  const PersonForm = (props) => {
+    return (
+      <form onSubmit={props.addName}>
+        <div>
+          name: <input value={props.newName} onChange={props.handleNameChange} />
+        </div>
+        <div>
+          number: <input value={props.newNumber} onChange={props.handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    )
+  }
+  const Persons = ({searchresults}) => {
+    return (
+      <ul style= {{display:"table-row"}}>
+        {searchresults.map(item => (
+          <li key={item.name}>
+            {item.name} {item.number}
+            </li>
+          ))}      
+      </ul>
+    )
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchName={searchName} handleSearch={handleSearch} />
+      <h2>add a new</h2>
+      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <h2>Numbers</h2>
+      <Persons searchresults={searchresults} />
+    </div>
+  )
+}
+
+export default App
+ReactDOM.render(<App />, document.getElementById("root"));
+
