693e618875d598fc0f636aa0d18681e292688664
Gianluca <giallotech@gmail.com>
2021-01-29 22:03:43 +0100

    Add new exercises

--- part_4/testing_step2/utils/list_helper.js
+++ part_4/testing_step2/utils/list_helper.js
@@ -1,9 +1,9 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   return 1
 }
 
-const totalLikes = (blogs) => {
-  const likes = blogs.map((item) => item.likes)
+const totalLikes = blogs => {
+  const likes = blogs.map(item => item.likes)
   const reducer = (accumulator, currentValue) => accumulator + currentValue
   return likes.reduce(reducer)
 }
