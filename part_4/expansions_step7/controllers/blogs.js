8301dcdd9652daa4f5ebb72c435eacd9fa34c95a
Gianluca <giallotech@gmail.com>
2021-02-06 14:01:32 +0100

    Committing new exercises and improvements

---
+++ part_4/expansions_step7/controllers/blogs.js
@@ -0,0 +1,63 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({
+  }).populate('user')
+  response.json(blogs)
+})
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
+blogsRouter.post('/', async (request, response) => {
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid'
+    })
+  }
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    likes: request.body.likes,
+    url: request.body.url,
+    user: user._id,
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true
+  })
+  response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
